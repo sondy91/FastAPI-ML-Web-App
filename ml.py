3cd2c9436db14ff55548b0ec1b6cf439428a6aa8
Austin Sonderman <6412733+sondy91@users.noreply.github.com>
2023-01-28 22:45:44 -0600

    UPdates

--- ml.py
+++ ml.py
@@ -23,3 +23,22 @@ prompt = "a photograph of an astronaut riding a horse"
 
 # you can save the image with
 # image.save(f"astronaut_rides_horse.png")
+
+def obtain_image(
+    prompt: str,
+    *,
+    seed: int | None = None,
+    num_inference_steps: int = 50,
+    guidance_scale: float = 7.5
+) -> Image:
+    generator = None if seed is None else torch.Generator("cuda").manual_seed(seed)
+    print(f"Using device: {pipe.device}")
+    image: Image = pipe(
+        prompt,
+        guidance_scale=guidance_scale,
+        num_inference_steps=num_inference_steps,
+        generator=generator,
+    ).images[0]
+    return image
+
+#image = obtain_image(prompt, num_inference_steps=5, seed=1024)
